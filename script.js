654ccbed4ffb7581ae071683959200927b7fc3c6
Evgenii <evgageev98@mail.ru>
2021-05-04 13:35:47 +0500

    firt try

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// Урок 005
+// let number = 5;
+// const myName = 'Evgenii';
+
+// number = 15;
+// console.log(number);
+
+// console.log(myName);
+
+// Урок 006
+// const persone = {
+//    name: 'Ivan',
+//    age: 25,
+//    isMarried: false
+// }
+
+// alert(persone.name);
+
+// let friendName = ['Alex', 'Danil', 'Vlad'];
+
+// console.log(friendName[2]);
+
+// Урок 007
+// const answers = [];
+
+// answers[0] = prompt('Погоняло?', '');
+// answers[1] = prompt('Сколько лет?', '');
+// answers[2] = prompt('Кто по жизни?', '');
+
+// console.log(answers);
+// console.log(typeof (answers));
+
+// Урок 008 Интерполяция (ES6)
+// const category = 'toys';
+// console.log(`https://url.com/${category}/5`);
+
+// const user = 69;
+// alert(`Привет, ${user}`)
+// console.log(typeof (user));
+
+// Урок 009 Операторы
+// И && true + false = false;
+// Или || true + false = true;
+// Отрицание != ;
+// ++incr --decr ;
+// == сравнение по значению/=== по значению + типу;
+
+// Урок 010 Git и GitHub
